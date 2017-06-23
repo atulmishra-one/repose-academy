6b3464440e40aa9909b0b754b5d4b2550f3f3924
atulmishra-one <atulmishra.one@gmail.com>
2017-06-23 17:46:38 +0530

    add model

--- contactus/admin.py
+++ contactus/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import ContactUs
+
+
+class ContactUsAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        return False
+
+
+admin.site.register(ContactUs, ContactUsAdmin)
